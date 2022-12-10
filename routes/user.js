201c4297e6f7481f20afa237497a15470307286a
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-09 22:01:39 -0800

    adding user put route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,27 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
-router.get("/usertest", (req, res) => {
-    res.send("user test is successful");
-});
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
 
-router.post("/userposttest", (req, res) => {
-    const username = req.body.username;
-    res.send(`Username is: ${username}`);
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id, 
+            {
+            $set: req.body
+            }, 
+            {new: true}
+        );
+        res.status(200).json(updatedUser);
+    } catch(err) {
+        res.status(500).json(err);
+    };
 });
 
 module.exports = router;

455c6168a58f792d97170944cd5545de7754e75e
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-12 16:40:27 -0800

    add product routes

--- routes/user.js
+++ routes/user.js
@@ -35,6 +35,17 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
       res.status(500).json(err);
     }
 });
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
   
 //GET ALL USERS
 router.get("/", verifyTokenAndAdmin, async (req, res) => {

45702ac6696cc820f5090558b647bf1d1cf5d94f
Tyler Pritchard <pritchard.tyler@gmail.com>
2023-01-05 17:18:26 -0800

    update Instructor name to title in model

--- models/Instructor.js
+++ models/Instructor.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const InstructorSchema = new mongoose.Schema(
     {
-        name:{
+        title:{
             type: String, 
             required: true,
         },
